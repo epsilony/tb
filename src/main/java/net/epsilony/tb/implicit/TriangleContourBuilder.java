beccebe3032c5076f54b97c28de70f2ef451d411
Man YUAN <epsilonyuan@gmail.com>
2013-05-29 13:40:31 +0800

    non-functional

--- src/main/java/net/epsilony/tb/implicit/TriangleContourBuilder.java
+++ src/main/java/net/epsilony/tb/implicit/TriangleContourBuilder.java
@@ -1,7 +1,6 @@
 /* (c) Copyright by Man YUAN */
 package net.epsilony.tb.implicit;
 
-import net.epsilony.tb.implicit.TriangleContourCell;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
