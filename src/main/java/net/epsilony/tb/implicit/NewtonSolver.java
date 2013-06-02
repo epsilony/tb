814645b8b5894f287da13a63656d4e0a93626543
Man YUAN <epsilonyuan@gmail.com>
2013-06-02 10:16:00 +0800

    add NewtonSolver and its tests in package implicit

---
+++ src/main/java/net/epsilony/tb/implicit/NewtonSolver.java
@@ -0,0 +1,156 @@
+/* (c) Copyright by Man YUAN */
+package net.epsilony.tb.implicit;
+
+import java.util.Arrays;
+import net.epsilony.tb.analysis.DifferentiableFunction;
+
+/**
+ *
+ * @author <a href="mailto:epsilonyuan@gmail.com">Man YUAN</a>
+ */
+public class NewtonSolver {
+
+    private double gradSq;
+
+    public enum SolutionStatus {
+
+        GOOD, DIVERGENT
+    };
+    public static double DEFAULT_ABSOLUTE_ERROR = 1E-6;
+    public static int DEFAULT_MAX_EVAL = 10000;
+    public static boolean DEFAULT_CHECK_FUNCTION_VALUE = false;
+    DifferentiableFunction<double[], double[]> function;
+    double absoluteError = DEFAULT_ABSOLUTE_ERROR;
+    int maxEval = DEFAULT_MAX_EVAL;
+    private double[] solution;
+    private double solutionError;
+    private double[] functionValue;
+    private int funcInputDimension;
+    int eval;
+    SolutionStatus solutionStatus;
+    private boolean checkFunctionValue = DEFAULT_CHECK_FUNCTION_VALUE;
+
+    public void setFunctionValue(double[] functionValue) {
+        this.functionValue = functionValue;
+    }
+
+    private boolean isCheckFunctionValue() {
+        return checkFunctionValue;
+    }
+
+    public DifferentiableFunction<double[], double[]> getFunction() {
+        return function;
+    }
+
+    public void setFunction(DifferentiableFunction<double[], double[]> function) {
+
+        if (1 != function.getOutputDimension()) {
+            throw new IllegalArgumentException();
+        }
+
+        this.function = function;
+        funcInputDimension = function.getInputDimension();
+    }
+
+    public double getAbsoluteError() {
+        return absoluteError;
+    }
+
+    public void setAbsoluteError(double absoluteError) {
+        this.absoluteError = absoluteError;
+    }
+
+    public int getMaxEval() {
+        return maxEval;
+    }
+
+    public void setMaxEval(int maxEval) {
+        this.maxEval = maxEval;
+    }
+
+    public boolean solve(double[] start) {
+        solution = Arrays.copyOf(start, start.length);
+        eval = 0;
+        do {
+            iterate();
+            eval++;
+        } while (keepIteration());
+        if (solutionStatus == SolutionStatus.GOOD) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public double[] getSolution() {
+        return solution;
+    }
+
+    public SolutionStatus getSolutionStatus() {
+        return solutionStatus;
+    }
+
+    private void iterate() {
+        functionValue = function.value(solution, functionValue);
+
+        solutionError = 0;
+        if (functionValue[0] == 0) {
+            return;
+        }
+        calcGradientSq();
+        if (0 == gradSq) {
+            return;
+        }
+        double value = -functionValue[0];
+        for (int i = 0; i < funcInputDimension; i++) {
+            double d = value / gradSq * functionValue[i + 1];
+            solutionError += d * d;
+            solution[i] += d;
+        }
+        solutionError = Math.sqrt(solutionError);
+    }
+
+    public double[] getFunctionValue() {
+        return functionValue;
+    }
+
+    public int getEvalTimes() {
+        return eval;
+    }
+
+    public double getSolutionError() {
+        return solutionError;
+    }
+
+    private double calcGradientSq() {
+        gradSq = 0;
+        for (int i = 1; i <= funcInputDimension; i++) {
+            double v = functionValue[i];
+            gradSq += v * v;
+        }
+        return gradSq;
+    }
+
+    private boolean keepIteration() {
+        if (gradSq == 0) {
+            if (Math.abs(functionValue[0]) <= absoluteError) {
+                solutionStatus = SolutionStatus.GOOD;
+            } else {
+                solutionStatus = SolutionStatus.DIVERGENT;
+            }
+            return false;
+        }
+        if (solutionError <= absoluteError) {
+            if (isCheckFunctionValue() && Math.abs(functionValue[0]) > absoluteError && eval < maxEval) {
+                return true;
+            }
+            solutionStatus = SolutionStatus.GOOD;
+            return false;
+        }
+        if (eval >= maxEval) {
+            solutionStatus = SolutionStatus.DIVERGENT;
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/test/java/net/epsilony/tb/implicit/NewtonSolverTest.java
@@ -0,0 +1,169 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.epsilony.tb.implicit;
+
+import net.epsilony.tb.analysis.DifferentiableFunction;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author epsilon
+ */
+public class NewtonSolverTest {
+
+    public NewtonSolverTest() {
+    }
+
+    public static class Quadric1D implements DifferentiableFunction<double[], double[]> {
+
+        @Override
+        public int getInputDimension() {
+            return 1;
+        }
+
+        @Override
+        public int getOutputDimension() {
+            return 1;
+        }
+
+        @Override
+        public double[] value(double[] input, double[] output) {
+            if (null == output) {
+                output = new double[2];
+            }
+            double x = input[0];
+            output[0] = x * x;
+            output[1] = 2 * x;
+            return output;
+        }
+
+        @Override
+        public int getDiffOrder() {
+            return 1;
+        }
+
+        @Override
+        public void setDiffOrder(int diffOrder) {
+            if (diffOrder != 1) {
+                throw new IllegalArgumentException("Only support 1 for testing");
+            }
+        }
+    }
+
+    //@Test
+    public void test1DQuadric() {
+        NewtonSolver solver = new NewtonSolver();
+        solver.setFunction(new Quadric1D());
+        solver.solve(new double[]{1000});
+        assertEquals(0, solver.getSolution()[0], 1e-6);
+        assertTrue(solver.getSolutionStatus() == NewtonSolver.SolutionStatus.GOOD);
+        assertTrue(solver.getEvalTimes() <= 30);
+        assertTrue(Math.abs(solver.getFunctionValue()[0]) < 1e-6);
+
+        solver.solve(new double[]{0});
+    }
+
+    public static class Quadric2D implements DifferentiableFunction<double[], double[]> {
+
+        @Override
+        public int getInputDimension() {
+            return 2;
+        }
+
+        @Override
+        public int getOutputDimension() {
+            return 1;
+        }
+
+        @Override
+        public double[] value(double[] input, double[] output) {
+            if (output == null) {
+                output = new double[3];
+            }
+            double x = input[0];
+            double y = input[1];
+
+            double value = (x - 1.5) * (x - 1.5) / 4 + (y + 2.3) * (y + 2.3);
+            double dx = (x - 1.5) / 2;
+            double dy = (y + 2.3) * 2;
+            output[0] = value;
+            output[1] = dx;
+            output[2] = dy;
+            return output;
+        }
+
+        @Override
+        public int getDiffOrder() {
+            return 1;
+        }
+
+        @Override
+        public void setDiffOrder(int diffOrder) {
+            if (diffOrder != 1) {
+                throw new IllegalArgumentException("Only support 1 for testing");
+            }
+        }
+    }
+
+    //@Test
+    public void test2DQuadric() {
+        NewtonSolver solver = new NewtonSolver();
+        solver.setFunction(new Quadric2D());
+        solver.solve(new double[]{1000, 1000});
+        solver.setMaxEval(50);
+        assertEquals(1.5, solver.getSolution()[0], 1e-6);
+        assertEquals(-2.3, solver.getSolution()[1], 1e-6);
+        assertTrue(solver.getSolutionStatus() == NewtonSolver.SolutionStatus.GOOD);
+        assertTrue(Math.abs(solver.getFunctionValue()[0]) < 1e-6);
+    }
+
+    public static class Quadric1DLocalMini implements DifferentiableFunction<double[], double[]> {
+
+        @Override
+        public int getInputDimension() {
+            return 1;
+        }
+
+        @Override
+        public int getOutputDimension() {
+            return 1;
+        }
+
+        @Override
+        public double[] value(double[] input, double[] output) {
+            if (null == output) {
+                output = new double[2];
+            }
+            double x = input[0];
+            output[0] = x * x + 1;
+            output[1] = 2 * x;
+            return output;
+        }
+
+        @Override
+        public int getDiffOrder() {
+            return 1;
+        }
+
+        @Override
+        public void setDiffOrder(int diffOrder) {
+            if (diffOrder != 1) {
+                throw new IllegalArgumentException("Only support 1 for testing");
+            }
+        }
+    }
+
+    @Test
+    public void testQuadric1DLocalMini() {
+        NewtonSolver solver = new NewtonSolver();
+        solver.setFunction(new Quadric1DLocalMini());
+        solver.solve(new double[]{0.1});
+        solver.setMaxEval(50);
+        assertEquals(NewtonSolver.SolutionStatus.DIVERGENT, solver.getSolutionStatus());
+        solver.solve(new double[]{0});
+        assertEquals(NewtonSolver.SolutionStatus.DIVERGENT, solver.getSolutionStatus());
+    }
+}
