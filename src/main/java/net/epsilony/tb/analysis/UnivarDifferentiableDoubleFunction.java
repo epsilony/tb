7ec112faf7be02faa1f02fe6e34fa731b8982368
Man YUAN <epsilonyuan@gmail.com>
2013-06-02 17:17:16 +0800

    add UnivarDifferentiableDoubleFunction interface

---
+++ src/main/java/net/epsilony/tb/analysis/UnivarDifferentiableDoubleFunction.java
@@ -0,0 +1,11 @@
+/* (c) Copyright by Man YUAN */
+package net.epsilony.tb.analysis;
+
+/**
+ *
+ * @author <a href="mailto:epsilonyuan@gmail.com">Man YUAN</a>
+ */
+public interface UnivarDifferentiableDoubleFunction extends WithDiffOrder {
+
+    double value(double x);
+}
