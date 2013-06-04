478e1a46f7f0b035d76798b88be82ee25a26fe35
Man YUAN <epsilonyuan@gmail.com>
2013-06-04 20:57:01 +0800

    move CircleTest in mf to tb and rename to CircleLevelSetTest

---
+++ src/test/java/net/epsilony/tb/implicit/CircleLevelSetTest.java
@@ -0,0 +1,43 @@
+/* (c) Copyright by Man YUAN */
+package net.epsilony.tb.implicit;
+
+import net.epsilony.tb.implicit.CircleLevelSet;
+import net.epsilony.tb.solid.ArcSegment2D;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author <a href="mailto:epsilonyuan@gmail.com">Man YUAN</a>
+ */
+public class CircleLevelSetTest {
+
+    public CircleLevelSetTest() {
+    }
+
+    @Test
+    public void testToArcs() {
+        double[] center = new double[]{-11.2, 2.3};
+        double radius = 4;
+        int expArcNum = 8;
+        CircleLevelSet circle = new CircleLevelSet(center[0], center[1], radius);
+        ArcSegment2D head = circle.toArcs(4);
+        ArcSegment2D seg = head;
+        int arcNum = 0;
+        while (true) {
+            assertArrayEquals(center, seg.calcCenter(null), 1e-14);
+            double actAngle = seg.calcStartAmplitudeAngle();
+            double expAngle = Math.PI * 2 / expArcNum * arcNum;
+            if (expAngle > Math.PI) {
+                expAngle -= Math.PI * 2;
+            }
+            assertEquals(expAngle, actAngle, 1e-10);
+            arcNum++;
+            seg = (ArcSegment2D) seg.getSucc();
+            if (seg == head) {
+                break;
+            }
+        }
+        assertEquals(expArcNum, arcNum);
+    }
+}
