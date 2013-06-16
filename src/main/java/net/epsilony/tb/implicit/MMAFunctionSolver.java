c7e31830f07d9a70982860f4eecf8e00f2143163
Man YUAN <epsilonyuan@gmail.com>
2013-06-16 16:38:57 +0800

    add MMAFunctionSolver and its tests

---
+++ src/main/java/net/epsilony/tb/implicit/MMAFunctionSolver.java
@@ -0,0 +1,132 @@
+/* (c) Copyright by Man YUAN */
+package net.epsilony.tb.implicit;
+
+import java.util.Arrays;
+import net.epsilony.tb.analysis.DifferentiableFunction;
+import net.epsilony.tb.nlopt.NloptAdapter;
+import net.epsilony.tb.nlopt.NloptLibrary;
+import static net.epsilony.tb.nlopt.NloptLibrary.*;
+import org.bridj.IntValuedEnum;
+
+/**
+ *
+ * @author <a href="mailto:epsilonyuan@gmail.com">Man YUAN</a>
+ */
+public class MMAFunctionSolver implements ImplicitFunctionSolver {
+
+    public static double DEFAULT_ABSOLUTE_FUNCTION_TOLERENCE = 1E-4;
+    NloptAdapter nloptAdapter;
+    double[] x;
+    double[] solution;
+    double[] oriValue;
+    DifferentiableFunction<double[], double[]> oriFunction;
+    DifferentiableFunction<double[], double[]> squaredFunction = new SquaredFunction();
+
+    private class SquaredFunction implements DifferentiableFunction<double[], double[]> {
+
+        @Override
+        public int getInputDimension() {
+            return oriFunction.getInputDimension();
+        }
+
+        @Override
+        public int getOutputDimension() {
+            return oriFunction.getOutputDimension();
+        }
+
+        @Override
+        public int getDiffOrder() {
+            return oriFunction.getDiffOrder();
+        }
+
+        @Override
+        public void setDiffOrder(int diffOrder) {
+            oriFunction.setDiffOrder(diffOrder);
+        }
+
+        @Override
+        public double[] value(double[] input, double[] output) {
+            oriValue = oriFunction.value(input, oriValue);
+            double t = oriValue[0] * 2;
+            for (int i = 1; i < oriValue.length; i++) {
+                output[i] = t * oriValue[i];
+            }
+            output[0] = oriValue[0] * oriValue[0];
+            return output;
+        }
+    }
+
+    public MMAFunctionSolver(int dimension) {
+        nloptAdapter = new NloptAdapter(NloptAlgorithm.NLOPT_LD_MMA, dimension);
+        _setFunctionAbsoluteTolerence(DEFAULT_ABSOLUTE_FUNCTION_TOLERENCE);
+    }
+
+    @Override
+    public double getFunctionAbsoluteTolerence() {
+        return Math.sqrt(nloptAdapter.getAbsoluteFunctionTolerence());
+    }
+
+    @Override
+    public double[] getFunctionValue() {
+        return oriValue;
+    }
+
+    @Override
+    public int getMaxEval() {
+        return nloptAdapter.getMaxEval();
+    }
+
+    @Override
+    public double[] getSolution() {
+        return nloptAdapter.getSolution();
+    }
+
+    @Override
+    public double getSolutionAbsoluteTolerence() {
+        return Math.sqrt(nloptAdapter.getStopValue());
+    }
+
+    @Override
+    public void setFunction(DifferentiableFunction<double[], double[]> function) {
+        oriFunction = function;
+        nloptAdapter.setMinObjective(squaredFunction);
+    }
+
+    @Override
+    public void setFunctionAbsoluteTolerence(double functionAbsoluteTolerence) {
+        _setFunctionAbsoluteTolerence(functionAbsoluteTolerence);
+    }
+
+    private void _setFunctionAbsoluteTolerence(double functionAbsoluteTolerence) {
+        double tol = functionAbsoluteTolerence * functionAbsoluteTolerence;
+        if (functionAbsoluteTolerence < 0) {
+            tol = -tol;
+
+        }
+        nloptAdapter.setStopValue(tol);
+    }
+
+    @Override
+    public void setMaxEval(int maxEval) {
+        nloptAdapter.setMaxEval(maxEval);
+    }
+
+    @Override
+    public void setSolutionAbsoluteTolerence(double solutionAbsoluteTolerence) {
+        nloptAdapter.setAbsoluteXTolerence(solutionAbsoluteTolerence);
+    }
+
+    @Override
+    public boolean solve(double[] start) {
+        oriValue = null;
+        double[] startCopy = Arrays.copyOf(start, start.length);
+        IntValuedEnum<NloptLibrary.NloptResult> nloptResult = nloptAdapter.optimize(startCopy, null);
+        if (nloptResult.value() > 0
+                && nloptResult.value() != NloptResult.NLOPT_MAXEVAL_REACHED.value
+                && nloptResult.value() != NloptResult.NLOPT_MAXTIME_REACHED.value) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ src/test/java/net/epsilony/tb/implicit/MMAFunctionSolverTest.java
@@ -0,0 +1,134 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.epsilony.tb.implicit;
+
+import java.util.Arrays;
+import net.epsilony.tb.analysis.AbstractDifferentiableFunction;
+import net.epsilony.tb.analysis.DifferentiableFunction;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author epsilon
+ */
+public class MMAFunctionSolverTest {
+
+    public MMAFunctionSolverTest() {
+    }
+
+    public static class Linear1D extends AbstractDifferentiableFunction {
+
+        final static double k = 3;
+        final static double x0 = 10;
+        final static double y0 = -3;
+        int count = 0;
+
+        @Override
+        public int getInputDimension() {
+            return 1;
+        }
+
+        @Override
+        public int getOutputDimension() {
+            return 1;
+        }
+
+        @Override
+        public double[] value(double[] input, double[] output) {
+            if (null == output) {
+                output = new double[1 + diffOrder];
+            }
+            double x = input[0];
+            double y = k * (x - x0) + y0;
+            output[0] = y;
+            if (diffOrder >= 1) {
+                output[1] = k;
+            }
+            System.out.println("evalTime = " + ++count);
+            return output;
+        }
+    }
+
+    @Test
+    public void testLinear1D() {
+        double tol = 1e-3;
+        MMAFunctionSolver solver = new MMAFunctionSolver(1);
+        DifferentiableFunction<double[], double[]> func = new Linear1D();
+        solver.setFunction(func);
+        solver.setMaxEval(200);
+        solver.setFunctionAbsoluteTolerence(tol);
+        solver.solve(new double[]{1});
+        double[] functionValue = solver.getFunctionValue();
+        double[] solution = solver.getSolution();
+        final double exp = -Linear1D.y0 / Linear1D.k + Linear1D.x0;
+        assertEquals(exp, solution[0], tol);
+        assertArrayEquals(functionValue, func.value(new double[]{exp}, null), tol);
+    }
+
+    public static class Quadric2D implements DifferentiableFunction<double[], double[]> {
+
+        int count = 0;
+
+        @Override
+        public int getInputDimension() {
+            return 2;
+        }
+
+        @Override
+        public int getOutputDimension() {
+            return 1;
+        }
+
+        @Override
+        public double[] value(double[] input, double[] output) {
+            if (output == null) {
+                output = new double[3];
+            }
+            double x = input[0];
+            double y = input[1];
+
+            double value = (x - 1.5) * (x - 1.5) / 4 + (y + 2.3) * (y + 2.3);
+            double dx = (x - 1.5) / 2;
+            double dy = (y + 2.3) * 2;
+            output[0] = value;
+            output[1] = dx;
+            output[2] = dy;
+            System.out.println(++count + "in: " + Arrays.toString(input) + ", out: " + Arrays.toString(output));
+            return output;
+        }
+
+        @Override
+        public int getDiffOrder() {
+            return 1;
+        }
+
+        @Override
+        public void setDiffOrder(int diffOrder) {
+            if (diffOrder != 1) {
+                throw new IllegalArgumentException("Only support 1 for testing");
+            }
+        }
+    }
+
+    @Test
+    public void test2DQuadric() {
+        MMAFunctionSolver solver = new MMAFunctionSolver(2);
+        solver.setFunction(new Quadric2D());
+        solver.setMaxEval(200);
+        final double tol = 1e-6;
+        solver.setFunctionAbsoluteTolerence(-1);
+        solver.setSolutionAbsoluteTolerence(tol);
+        assertTrue(solver.solve(new double[]{1000, 1000}));
+        assertEquals(1.5, solver.getSolution()[0], tol * 10);
+        assertEquals(-2.3, solver.getSolution()[1], tol * 10);
+        assertTrue(Math.abs(solver.getFunctionValue()[0]) < tol);
+        
+        assertTrue(solver.solve(new double[]{20, -30}));
+        assertEquals(1.5, solver.getSolution()[0], tol * 10);
+        assertEquals(-2.3, solver.getSolution()[1], tol * 10);
+        assertTrue(Math.abs(solver.getFunctionValue()[0]) < tol);
+    }
+}
