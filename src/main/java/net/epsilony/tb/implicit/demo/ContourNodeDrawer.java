49771432e6e73640f8b15fd369094730cc336c3e
Man YUAN <epsilonyuan@gmail.com>
2013-06-18 19:31:32 +0800

    add ContourNodeDrawer

---
+++ src/main/java/net/epsilony/tb/implicit/demo/ContourNodeDrawer.java
@@ -0,0 +1,120 @@
+/* (c) Copyright by Man YUAN */
+package net.epsilony.tb.implicit.demo;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.Stroke;
+import java.awt.geom.Line2D;
+import java.util.Arrays;
+import net.epsilony.tb.analysis.Math2D;
+import net.epsilony.tb.implicit.ContourNode;
+import net.epsilony.tb.solid.Node;
+import net.epsilony.tb.solid.ui.NodeDrawer;
+
+/**
+ *
+ * @author <a href="mailto:epsilonyuan@gmail.com">Man YUAN</a>
+ */
+public class ContourNodeDrawer extends NodeDrawer {
+
+    public static Color DEFAULT_GRADIENT_COLOR = new Color(0, 0, 255, 160);
+    public static Stroke DEFAULT_GRADIENT_STROKE = new BasicStroke(0.5f);
+    public static int DEFAULT_UNIT_GRADIENT_LENGTH = 25;
+    private boolean unitGradient = true;
+    private int unitGradientLength = DEFAULT_UNIT_GRADIENT_LENGTH;
+    private boolean drawGradient = true;
+    private double gradientScale = 0.1;
+    private Color gradientColor = DEFAULT_GRADIENT_COLOR;
+    private Stroke gradientStroke = DEFAULT_GRADIENT_STROKE;
+
+    public boolean isDrawGradient() {
+        return drawGradient;
+    }
+
+    public void setDrawGradient(boolean drawGradient) {
+        this.drawGradient = drawGradient;
+    }
+
+    @Override
+    public void drawModel(Graphics2D g2) {
+        super.drawModel(g2);
+        Node node = getNode();
+        if (drawGradient && node instanceof ContourNode) {
+            drawGradient((ContourNode) node, g2);
+        }
+    }
+
+    private void drawGradient(ContourNode node, Graphics2D g2) {
+        double[] functionValue = node.getFunctionValue();
+        if (functionValue == null || functionValue.length != 3) {
+            return;
+        }
+        double[] coord = node.getCoord();
+        double[] nodePosition = new double[2];
+        modelToComponentTransform.transform(coord, 0, nodePosition, 0, 1);
+        double[] gradient = Arrays.copyOfRange(functionValue, 1, 3);
+        Shape line;
+        if (unitGradient) {
+            Math2D.normalize(gradient, gradient);
+            Math2D.scale(gradient, unitGradientLength, gradient);
+            line = new Line2D.Double(
+                    nodePosition[0], nodePosition[1],
+                    nodePosition[0] + gradient[0], nodePosition[1] - gradient[1]);
+
+        } else {
+            Math2D.scale(gradient, gradientScale, gradient);
+            line=new Line2D.Double(coord[0], coord[1], coord[0]+gradient[0], coord[1]+gradient[1]);
+            line=modelToComponentTransform.createTransformedShape(line);
+        }
+        
+        Color oldColor = g2.getColor();
+        Stroke oldStroke = g2.getStroke();
+        g2.setStroke(gradientStroke);
+        g2.setColor(gradientColor);
+        g2.draw(line);
+        g2.setColor(oldColor);
+        g2.setStroke(oldStroke);
+    }
+
+    public boolean isUnitGradient() {
+        return unitGradient;
+    }
+
+    public void setUnitGradient(boolean unitGradient) {
+        this.unitGradient = unitGradient;
+    }
+
+    public int getUnitGradientLength() {
+        return unitGradientLength;
+    }
+
+    public void setUnitGradientLength(int unitGradientLength) {
+        this.unitGradientLength = unitGradientLength;
+    }
+
+    public double getGradientScale() {
+        return gradientScale;
+    }
+
+    public void setGradientScale(double gradientScale) {
+        this.gradientScale = gradientScale;
+    }
+
+    public Color getGradientColor() {
+        return gradientColor;
+    }
+
+    public void setGradientColor(Color gradientColor) {
+        this.gradientColor = gradientColor;
+    }
+
+    public Stroke getGradientStroke() {
+        return gradientStroke;
+    }
+
+    public void setGradientStroke(Stroke gradientStroke) {
+        this.gradientStroke = gradientStroke;
+    }
+}
