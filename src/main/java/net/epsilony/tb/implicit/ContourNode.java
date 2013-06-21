01013dc097a1f570143c5306c6cb0a9eca5d8223
Man YUAN <epsilonyuan@gmail.com>
2013-06-21 15:02:55 +0800

    add ContourNode.gradientCos()

--- src/main/java/net/epsilony/tb/implicit/ContourNode.java
+++ src/main/java/net/epsilony/tb/implicit/ContourNode.java
@@ -1,6 +1,7 @@
 /* (c) Copyright by Man YUAN */
 package net.epsilony.tb.implicit;
 
+import net.epsilony.tb.analysis.Math2D;
 import net.epsilony.tb.solid.Node;
 
 /**
@@ -17,5 +18,15 @@ public class ContourNode extends Node {
 
     public void setFunctionValue(double[] functionValue) {
         this.functionValue = functionValue;
+    }
+
+    public static double gradientCos(ContourNode ndA, ContourNode ndB) {
+        double[] funa = ndA.functionValue;
+        double[] funb = ndB.functionValue;
+        double gax = funa[1];
+        double gay = funa[2];
+        double gbx = funb[1];
+        double gby = funb[2];
+        return Math2D.cos(gax, gay, gbx, gby);
     }
 }
