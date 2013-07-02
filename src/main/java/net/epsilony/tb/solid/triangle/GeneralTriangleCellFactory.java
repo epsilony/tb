0126e1794790adf5e513c795809c93aafd8da200
Man YUAN <epsilonyuan@gmail.com>
2013-07-02 21:19:42 +0800

    add GeneralTriangleCellFactory

---
+++ src/main/java/net/epsilony/tb/solid/triangle/GeneralTriangleCellFactory.java
@@ -0,0 +1,68 @@
+/* (c) Copyright by Man YUAN */
+package net.epsilony.tb.solid.triangle;
+
+import net.epsilony.tb.Factory;
+import net.epsilony.tb.solid.Node;
+import net.epsilony.tb.solid.Segment2DUtils;
+import net.epsilony.tb.solid.WingedEdge;
+
+/**
+ *
+ * @author <a href="mailto:epsilonyuan@gmail.com">Man YUAN</a>
+ */
+public class GeneralTriangleCellFactory<
+        CELL extends TriangleCell<CELL, EDGE, NODE>, //
+        EDGE extends WingedEdge<CELL, EDGE, NODE>, //
+        NODE extends Node> implements Factory<CELL> {
+
+    Factory<? extends CELL> cellFactory;
+    Factory<? extends EDGE> edgeFactory;
+    Factory<? extends NODE> nodeFactory;
+
+    public GeneralTriangleCellFactory(Factory<? extends CELL> cellFactory, Factory<? extends EDGE> edgeFactory, Factory<? extends NODE> nodeFactory) {
+        this.cellFactory = cellFactory;
+        this.edgeFactory = edgeFactory;
+        this.nodeFactory = nodeFactory;
+    }
+
+    public GeneralTriangleCellFactory() {
+    }
+
+    @Override
+    public CELL produce() {
+        CELL result = cellFactory.produce();
+        for (int i = 0; i < 3; i++) {
+            result.setVertexEdge(i, edgeFactory.produce());
+            result.setVertex(i, nodeFactory.produce());
+        }
+        for (int i = 0; i < 3; i++) {
+            Segment2DUtils.link(result.getVertexEdge(i), result.getVertexEdge((i + 1) % 3));
+            result.getVertexEdge(i).setCell(result);
+        }
+        return result;
+    }
+
+    public Factory<? extends CELL> getCellFactory() {
+        return cellFactory;
+    }
+
+    public void setCellFactory(Factory<? extends CELL> cellFactory) {
+        this.cellFactory = cellFactory;
+    }
+
+    public Factory<? extends EDGE> getEdgeFactory() {
+        return edgeFactory;
+    }
+
+    public void setEdgeFactory(Factory<? extends EDGE> edgeFactory) {
+        this.edgeFactory = edgeFactory;
+    }
+
+    public Factory<? extends NODE> getNodeFactory() {
+        return nodeFactory;
+    }
+
+    public void setNodeFactory(Factory<? extends NODE> nodeFactory) {
+        this.nodeFactory = nodeFactory;
+    }
+}
