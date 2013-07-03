a91514055219d10d6d58194ee09f64c5a77a0e98
Man YUAN <epsilonyuan@gmail.com>
2013-07-03 15:18:22 +0800

    add TriangleCellUtils

---
+++ src/main/java/net/epsilony/tb/solid/triangle/TriangleCellUtils.java
@@ -0,0 +1,31 @@
+/* (c) Copyright by Man YUAN */
+package net.epsilony.tb.solid.triangle;
+
+import net.epsilony.tb.solid.WingedEdge;
+import net.epsilony.tb.solid.WingedEdgeUtils;
+
+/**
+ *
+ * @author <a href="mailto:epsilonyuan@gmail.com">Man YUAN</a>
+ */
+public class TriangleCellUtils {
+
+    public static void linkOppositesBySameVertes(TriangleCell cellA, TriangleCell cellB) {
+        WingedEdge edgeA = null, edgeB = null;
+        boolean finded = false;
+        for (int i = 0; i < 3 && !finded; i++) {
+            edgeA = cellA.getVertexEdge(i);
+            for (int j = 0; j < 3; j++) {
+                edgeB = cellB.getVertexEdge(j);
+                if (edgeA.getStart() == edgeB.getEnd() && edgeA.getEnd() == edgeB.getStart()) {
+                    finded = true;
+                    break;
+                }
+            }
+        }
+        if (!finded) {
+            throw new IllegalArgumentException();
+        }
+        WingedEdgeUtils.linkAsOpposite(edgeA, edgeB);
+    }
+}
