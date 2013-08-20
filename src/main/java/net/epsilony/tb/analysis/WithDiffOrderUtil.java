9ed30d20f2e78cedc009cf086bdbb492137887ea
Man YUAN <epsilonyuan@gmail.com>
2013-08-20 13:30:54 +0800

    add 3d support and fix 1d bug of WithDiffOrderUtil.outputLength()

--- src/main/java/net/epsilony/tb/analysis/WithDiffOrderUtil.java
+++ src/main/java/net/epsilony/tb/analysis/WithDiffOrderUtil.java
@@ -2,6 +2,7 @@
 package net.epsilony.tb.analysis;
 
 import gnu.trove.list.array.TDoubleArrayList;
+import org.apache.commons.math3.util.ArithmeticUtils;
 
 /**
  *
@@ -12,9 +13,11 @@ public class WithDiffOrderUtil {
     public static int outputLength(int dim, int diffOrder) {
         switch (dim) {
             case 1:
-                return diffOrder;
+                return diffOrder + 1;
             case 2:
                 return outputLength2D(diffOrder);
+            case 3:
+                return outputLength3D(diffOrder);
             default:
                 throw new IllegalArgumentException("The dim should be 1 or 2, not " + dim);
         }
@@ -22,6 +25,18 @@ public class WithDiffOrderUtil {
 
     public static int outputLength2D(int diffOrder) {
         return (diffOrder + 2) * (diffOrder + 1) / 2;
+    }
+    private static final int[] OUTPUT_LENGTH_3D = new int[]{1, 4, 10, 20};
+
+    public static int outputLength3D(int diffOrder) {
+        if (diffOrder < OUTPUT_LENGTH_3D.length) {
+            return OUTPUT_LENGTH_3D[diffOrder];
+        }
+        int result = OUTPUT_LENGTH_3D[OUTPUT_LENGTH_3D.length - 1];
+        for (int i = OUTPUT_LENGTH_3D.length; i <= diffOrder; i++) {
+            result += ArithmeticUtils.binomialCoefficient(2 + i, 2);
+        }
+        return result;
     }
 
     public static TDoubleArrayList[] initOutput(TDoubleArrayList[] toInit, int capacity, int dim, int diffOrder) {
