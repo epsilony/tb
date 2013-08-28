5545b0a215ad2ee1cd5c0ffb6d7d323f44015f0f
Man YUAN <epsilonyuan@gmail.com>
2013-08-28 20:46:56 +0800

    rename to SynchronizedIterator and add estimatedSize field

---
+++ src/main/java/net/epsilony/tb/synchron/SynchronizedIterator.java
@@ -0,0 +1,40 @@
+/* (c) Copyright by Man YUAN */
+package net.epsilony.tb.synchron;
+
+import java.util.Iterator;
+
+/**
+ *
+ * @author <a href="mailto:epsilonyuan@gmail.com">Man YUAN</a>
+ */
+public class SynchronizedIterator<T> {
+
+    public SynchronizedIterator(Iterator<? extends T> iterator) {
+        this.iterator = iterator;
+    }
+
+    public SynchronizedIterator(Iterator<? extends T> iterator, int estimatedSize) {
+        this.iterator = iterator;
+        this.estimatedSize = estimatedSize;
+    }
+    Iterator<? extends T> iterator;
+    int estimatedSize = -1;
+    int count = 0;
+
+    public synchronized T nextItem() {
+        if (iterator.hasNext()) {
+            count++;
+            return iterator.next();
+        } else {
+            return null;
+        }
+    }
+
+    public synchronized int getEstimatedSize() {
+        return estimatedSize;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
