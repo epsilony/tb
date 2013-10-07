7db51d8b289f93883a0d33a3b98efeb39a591ac9
Man YUAN <epsilonyuan@gmail.com>
2013-10-07 14:49:58 +0800

    make GeomUnit Serializable

--- src/main/java/net/epsilony/tb/solid/GeomUnit.java
+++ src/main/java/net/epsilony/tb/solid/GeomUnit.java
@@ -1,13 +1,14 @@
 /* (c) Copyright by Man YUAN */
 package net.epsilony.tb.solid;
 
+import java.io.Serializable;
 import net.epsilony.tb.IntIdentity;
 
 /**
  *
  * @author <a href="mailto:epsilonyuan@gmail.com">Man YUAN</a>
  */
-public interface GeomUnit extends IntIdentity {
+public interface GeomUnit extends IntIdentity, Serializable {
 
     GeomUnit getParent();
 
